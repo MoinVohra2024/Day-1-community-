e6210444c834cab882c3f0fdfce5ed18a30d4463
moin vohra <moindeveloper2024@gmail.com>
2024-07-16 15:53:58 +0530

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,38 @@
+// task-1 variable declaration
+//task-1.1 {declare variable using var assign it number and console it}
+var number1 = 1233;
+console.log(number1);
+
+//task-1.2 {declare variable using let assign it string and console it}
+let strr1 = "moin vohra";
+console.log(strr1);
+//task 2
+//task-2.1 {declare variable using const assign it boolean and console it}
+
+const bool = (10 < 20);
+console.log(bool);
+//task 3
+//task-3.1 {declare variables of type :number,string,boolean,object , array :and using typeod operater find there's typeee}tooo easyyy.....!!!!
+
+let num1 = 10;
+let str1 = "moin";
+let bool1 = (20 > 10);
+let obj1 = {
+    name: "vohra moin",
+    age : 17
+};     
+let arr1 = ["blue","green","white"];
+
+console.log(typeof num1);
+console.log(typeof str1);
+console.log(typeof bool1);
+console.log(typeof obj1);
+console.log(typeof arr1);
+
+//task 2
+// re create variable of let and console bothh
+
+let re1 = 90;
+console.log(re1);
+const re1 = 100;
+console.log(re1);
